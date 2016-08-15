a777fac2eb65b4fe5732dd7850cbbc19ed491072
jffcole7 <jffcole7@gmail.com>
2016-08-14 20:17:05 -0500

    new file:   abba.py

---
+++ abba.py
@@ -0,0 +1,16 @@
+import sys
+def getOptionValue(option):
+    optionPos = [i for i, j in enumerate(sys.argv) if j == option][0]
+    optionValue = sys.argv[optionPos + 1]
+    return optionValue
+if "-i" in sys.argv:
+    fileName = getOptionValue("-i")
+else:
+    print "\nplease specify input file name using -i <file_name> \n"
+    sys.exit()
+
+
+with open(fileName) as textFile:
+    lines = [line.split() for line in textFile]
+
+print lines
