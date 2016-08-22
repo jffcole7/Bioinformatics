be9aec39f52f0d9edb7ba094e0e2d8260f969ec8
Jeff <jffcole7@gmail.com>
2016-08-22 11:22:49 -0400

    modified:   matrix.DE.sh

--- matrix.DE.sh
+++ matrix.DE.sh
@@ -1,15 +1,11 @@
 ~/transcriptome_programs/trinityrnaseq-2.2.0/util/abundance_estimates_to_matrix.pl --est_method kallisto \
 --out_prefix trans_counts \
 --name_sample_by_basedir \
-TstelarobustaWaikamoi/abundance.tsv \
-TstelarobustaWestmaui/abundance.tsv \
-TpaludicolaMolokai/abundance.tsv \
-TpaludicolaWestmaui/abundance.tsv
+Tacuta/abundance.tsv \
+Tpilosa/abundance.tsv 
 
 ~/transcriptome_programs/trinityrnaseq-2.2.0/util/abundance_estimates_to_matrix.pl --est_method kallisto \
 --out_prefix gene_counts \
 --name_sample_by_basedir \
-TstelarobustaWaikamoi/abundance.tsv.genes \
-TstelarobustaWestmaui/abundance.tsv.genes \
-TpaludicolaMolokai/abundance.tsv.genes \
-TpaludicolaWestmaui/abundance.tsv.genes 
+Tacuta/abundance.tsv.genes \
+Tpilosa/abundance.tsv.genes
