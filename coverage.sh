bca1ec91a15bd6717bc4acd954f0eff4a0fcffd9
Jeff <jffcole7@gmail.com>
2016-08-22 15:58:55 -0400

    modified:   coverage.sh

--- coverage.sh
+++ coverage.sh
@@ -1,4 +1,10 @@
+#for BAM in *.sorted.bam
+#do
+#java -jar /home/brewerlab/transcriptome_programs/picard-tools-1.119/CollectAlignmentSummaryMetrics.jar I=$BAM O=$BAM.stats
+#done
+
+
 for BAM in *.sorted.bam
 do
-java -jar /home/brewerlab/transcriptome_programs/picard-tools-1.119/CollectAlignmentSummaryMetrics.jar I=$BAM O=$BAM.stats
+java -Xmx4g -jar~/BAMStats-1.25/BAMStats-1.25.jar -i $BAM -o $BAM.BAMstats
 done
