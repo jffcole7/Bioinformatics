0b5f007d4c1e20123e6ef5ed8ae5d1c124c0de98
Jeff <jffcole7@gmail.com>
2016-08-22 11:37:24 -0400

    modified:   matrix.DE.sh

--- matrix.DE.sh
+++ matrix.DE.sh
@@ -1,11 +1,24 @@
 ~/transcriptome_programs/trinityrnaseq-2.2.0/util/abundance_estimates_to_matrix.pl --est_method kallisto \
 --out_prefix trans_counts \
 --name_sample_by_basedir \
-Tacuta/abundance.tsv \
-Tpilosa/abundance.tsv 
+Tpilosa/abundance.tsv \
+Tquasimodo1/abundance.tsv \
+Tquasimodo2/abundance.tsv \
+TstelarobustaWaikamoi/abundance.tsv \
+TstelarobustaWestmaui/abundance.tsv \
+TpaludicolaWestmaui/abundance.tsv \
+TpaludicolaMolokai/abundance.tsv \
+Tacuta/abundance.tsv
+
 
 ~/transcriptome_programs/trinityrnaseq-2.2.0/util/abundance_estimates_to_matrix.pl --est_method kallisto \
 --out_prefix gene_counts \
 --name_sample_by_basedir \
-Tacuta/abundance.tsv.genes \
-Tpilosa/abundance.tsv.genes
+Tpilosa/abundance.tsv.genes \
+Tquasimodo1/abundance.tsv.genes \
+Tquasimodo2/abundance.tsv.genes \
+TstelarobustaWaikamoi/abundance.tsv.genes \
+TstelarobustaWestmaui/abundance.tsv.genes \
+TpaludicolaWestmaui/abundance.tsv.genes \
+TpaludicolaMolokai/abundance.tsv.genes \
+Tacuta/abundance.tsv.genes
