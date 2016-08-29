331c08b61b70925a0a9ff98c73697c2e64636d7c
Jeff <jffcole7@gmail.com>
2016-08-29 11:46:38 -0400

    modified:   TJC_AbbaBaba_v2.3.2.py

--- TJC_AbbaBaba_v2.3.2.py
+++ TJC_AbbaBaba_v2.3.2.py
@@ -141,8 +141,9 @@ with open(fileName) as f:
 
             if chrom!=chrom_change:
                 blockNum=1
+                print "the new bloc will start at position",start_pos
                 print "******************\nThe chromosomejust changed from chromosome"+chrom_change+ "and position",prev_pos," to chromosome "+chrom+" and position ", pos," on the new chromosome\n************************"
-                start_pos=pos 
+                start_pos=pos
             prev_pos=pos
                 #print row
             #print row
