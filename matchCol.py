f739d1e40afb8df553dc2e35fc46104067f0a213
Jeff <jffcole7@gmail.com>
2016-09-15 18:04:53 -0400

    modified:   matchCol.py

--- matchCol.py
+++ matchCol.py
@@ -27,6 +27,6 @@ with open(index_file) as f:
 
 with open(go_file) as f:
     for line in f:
-        row = line.strip:
+        row = line.strip
         if row[0] in index_array:
-            print line 
+            print line
