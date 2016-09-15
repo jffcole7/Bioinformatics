662ed2e46b3f428c1b77a8b4511f70d6b492aa6e
Jeff <jffcole7@gmail.com>
2016-09-15 16:26:05 -0400

    new file:   fasta_subset.py

---
+++ fasta_subset.py
@@ -0,0 +1,38 @@
+import sys
+from Bio import SeqIO
+
+def getOptionValue(option):
+    optionPos = [i for i, j in enumerate(sys.argv) if j == option][0]
+    optionValue = sys.argv[optionPos + 1]
+    return optionValue
+if "-fasta" in sys.argv:
+    fasta_file = getOptionValue("-fasta") # Input fasta file
+else:
+    print "\nplease specify input file name using -i <file_name> \n"
+    sys.exit()
+
+if "-id" in sys.argv:
+    wanted_file = getOptionValue("-id") # Input fasta file
+else:
+    print "\nplease specify input file name using -i <file_name> \n"
+    sys.exit()
+
+if "-out" in sys.argv:
+    result_file = getOptionValue("-out") # Input fasta file
+else:
+    print "\nplease specify input file name using -i <file_name> \n"
+    sys.exit()
+
+
+wanted = set()
+with open(wanted_file) as f:
+    for line in f:
+        line = line.strip()
+        if line != "":
+            wanted.add(line)
+
+fasta_sequences = SeqIO.parse(open(fasta_file),'fasta')
+with open(result_file, "w") as f:
+    for seq in fasta_sequences:
+        if seq.id in wanted:
+            SeqIO.write([seq], f, "fasta")
