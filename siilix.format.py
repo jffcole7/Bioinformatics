e3bf21ac51b77e09b392a3860fdda42cb99ed0d0
jffcole7 <jffcole7@gmail.com>
2016-10-06 11:29:28 -0400

    new file:   siilix.format.py

---
+++ siilix.format.py
@@ -0,0 +1,25 @@
+import sys
+
+def getOptionValue(option):
+    optionPos = [i for i, j in enumerate(sys.argv) if j == option][0]
+    optionValue = sys.argv[optionPos + 1]
+    return optionValue
+
+if "-i" in sys.argv:
+    inputFile = getOptionValue("-i")
+else:
+    print "no"
+    sys.exit()
+
+familyNumber=1
+
+with open(inputFile) as f:
+    for line in f:
+        #print line
+
+
+        row = line.split()
+        for i in range(len(row)):
+            if len(row)>14:
+                print "FAM"+str(familyNumber),row[i]
+        familyNumber+=1
